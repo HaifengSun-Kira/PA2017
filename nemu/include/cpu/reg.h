af591162d95e8a6f5d76075d42d010bd7296f964
tracer-ics2017 <tracer@njuics.org>
2017-09-13 15:20:20 +0000

    >  compile 201728013229028 kira Linux 51c67a8429f8 4.4.0-93-generic #116-Ubuntu SMP Fri Aug 11 21:17:51 UTC 2017 x86_64 GNU/Linux  15:20:20 up 16:18,  6 users,  load average: 0.42, 0.39, 0.46 447ad08af08c4b056cf4b1cb45ff1af958bf67d6

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -36,6 +36,20 @@ typedef struct {
 
   vaddr_t eip;
 
+  union {
+	  uint32_t eflags_ini;
+	  struct {
+		uint32_t CF : 1;
+		uint32_t    : 5;
+		uint32_t ZF : 1;
+		uint32_t SF : 1;
+		uint32_t    : 1;
+		uint32_t IF : 1;				  
+		uint32_t    : 1;
+		uint32_t OF : 1;				  
+	  }eflags;
+  };
+
 } CPU_state;
 
 extern CPU_state cpu;
