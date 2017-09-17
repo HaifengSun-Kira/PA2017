b8c00a8e42811a3df862756667e46ee4d59a2fd5
tracer-ics2017 <tracer@njuics.org>
2017-09-17 05:53:58 +0000

    >  compile 201728013229028 kira Linux 51c67a8429f8 4.4.0-93-generic #116-Ubuntu SMP Fri Aug 11 21:17:51 UTC 2017 x86_64 GNU/Linux  05:53:58 up 12:24, 11 users,  load average: 0.01, 0.06, 0.08 dee42c0c4fd6f7b392ba88c2a7081bd74ad9f80f

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -134,9 +134,15 @@ static inline void rtl_not(rtlreg_t* dest) {
 	*dest = ~(*dest);
 }
 
+
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
-  TODO();
+	switch(width){
+		case 1: *dest = (int32_t) (int8_t) (uint8_t) *src1; return;
+		case 2: *dest = (int32_t) (int16_t) (uint16_t) *src1; return;
+		case 4: *dest = (int32_t) *src1; return;
+		default: assert(0);
+	}
 }
 
 static inline void rtl_push(const rtlreg_t* src1) {
