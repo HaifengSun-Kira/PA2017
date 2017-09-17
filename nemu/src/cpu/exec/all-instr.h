4834289a39c9c8535db34f9fd24926a707b2a111
tracer-ics2017 <tracer@njuics.org>
2017-09-17 05:48:13 +0000

    >  compile 201728013229028 kira Linux 51c67a8429f8 4.4.0-93-generic #116-Ubuntu SMP Fri Aug 11 21:17:51 UTC 2017 x86_64 GNU/Linux  05:48:13 up 12:19, 11 users,  load average: 0.24, 0.19, 0.12 bc85f8b17d501f4dd0708edaf0a2abbfde5339ca

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -18,6 +18,9 @@ make_EHelper(sub);
 make_EHelper(dec);
 make_EHelper(inc);
 make_EHelper(cmp);
+make_EHelper(imul1);
+make_EHelper(imul2);
+make_EHelper(imul3);
 
 make_EHelper(and);
 make_EHelper(not);
