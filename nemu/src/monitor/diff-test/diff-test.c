82a46e5ae18347adcde86518346ed99763f5fe50
tracer-ics2017 <tracer@njuics.org>
2017-09-17 14:00:21 +0000

    >  compile 201728013229028 kira Linux 51c67a8429f8 4.4.0-93-generic #116-Ubuntu SMP Fri Aug 11 21:17:51 UTC 2017 x86_64 GNU/Linux  14:00:21 up 20:31, 13 users,  load average: 0.15, 0.17, 0.14 7dddffa6b94a38f62521128796cca2c02428c21e

--- nemu/src/monitor/diff-test/diff-test.c
+++ nemu/src/monitor/diff-test/diff-test.c
@@ -149,7 +149,12 @@ void difftest_step(uint32_t eip) {
 
   // TODO: Check the registers state with QEMU.
   // Set `diff` as `true` if they are not the same.
-  TODO();
+  if(cpu.eax == r.eax && cpu.ebx == r.ebx && cpu.ecx == r.ecx && cpu.edx == r.edx
+	&& cpu.esi == r.esi && cpu.edi == r.esi && cpu.ebp == r.ebp && cpu.esp == r.esp
+	&& cpu.eip == r.eip) {
+  } else {
+	diff = true;
+  }
 
   if (diff) {
     nemu_state = NEMU_END;
