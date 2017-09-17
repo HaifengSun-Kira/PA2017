98f1ec1039a873c10a45782940048a9955d9638a
tracer-ics2017 <tracer@njuics.org>
2017-09-17 12:54:46 +0000

    >  compile 201728013229028 kira Linux 51c67a8429f8 4.4.0-93-generic #116-Ubuntu SMP Fri Aug 11 21:17:51 UTC 2017 x86_64 GNU/Linux  12:54:46 up 19:25, 13 users,  load average: 0.30, 0.20, 0.12 7802d5e21a92fab4ccb600cfb6e4edee9eea9c1

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -10,6 +10,7 @@ make_EHelper(leave);
 make_EHelper(cltd);
 
 make_EHelper(call);
+make_EHelper(call_rm);
 make_EHelper(ret);
 make_EHelper(jcc);
 make_EHelper(jmp);
--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -49,7 +49,16 @@ make_EHelper(ret) {
 }
 
 make_EHelper(call_rm) {
-  TODO();
+//  if(decoding.is_operand_size_16) {
+//	rtl_push(&decoding.seq_eip);
+//	decoding.jmp_eip = id_dest->val & 0x0000ffff;	
+ // } else {
+//	rtl_push(&decoding.seq_eip);
+//	decoding.jmp_eip = id_dest->val;	
+ // }
+  rtl_push(&decoding.seq_eip);
+  decoding.jmp_eip = id_dest->val;	
+  decoding.is_jmp = 1;
 
   print_asm("call *%s", id_dest->str);
 }
