8ef8efb783f5370fab5f431c4af2b660d531c910
tracer-ics2017 <tracer@njuics.org>
2017-09-21 03:40:08 +0000

    >  compile 201728013229028 kira Linux 51c67a8429f8 4.4.0-96-generic #119-Ubuntu SMP Tue Sep 12 14:59:54 UTC 2017 x86_64 GNU/Linux  03:40:08 up  3:30, 12 users,  load average: 0.18, 0.22, 0.21 4751e868c60922e82e9fb188ee6b64db78ef30b2

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -69,10 +69,21 @@ make_EHelper(sar) {
 }
 
 make_EHelper(rol) {
-	rtl_shri(&t2, &id_dest->val, id_dest->val * 8 - id_src->val);
+	rtl_shri(&t2, &id_dest->val, id_dest->width * 8 - id_src->val);
 	rtl_shl(&t3, &id_dest->val, &id_src->val);
 	rtl_or(&t1, &t2, &t3);
 	operand_write(id_dest, &t1);
+
+	if (id_src->val == 1) {
+		t2 = (t1 >> (id_dest->width * 8 -1)) & 0x1;
+		rtl_get_CF(&t0);
+		if (t2 == t0) {
+			rtl_set_OF(&tzero);
+		} else {
+			t2 = 1;
+			rtl_set_OF(&t2);
+		}
+	}
     
 	print_asm_template2(rol);
 }
