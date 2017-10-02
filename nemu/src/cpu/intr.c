853bb844a746ad3b5c68b67a980aa8f2777c38aa
tracer-ics2017 <tracer@njuics.org>
2017-10-02 05:21:04 +0000

    >  compile 201728013229028 kira Linux 51c67a8429f8 4.4.0-96-generic #119-Ubuntu SMP Tue Sep 12 14:59:54 UTC 2017 x86_64 GNU/Linux  05:21:04 up  8:10, 12 users,  load average: 0.20, 0.12, 0.10 4d4ea63a726b0150495018ae76d966b04775800a

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -9,6 +9,7 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
 	rtl_push((rtlreg_t *)&cpu.cs);
 	rtl_push((rtlreg_t *)&ret_addr);
 	uint32_t idt_base = cpu.idtr.base;
+	printf("idtbase 0x%-8x\n", idt_base);
 	uint32_t offset = vaddr_read(idt_base + NO * 64, 2);
 	offset += (vaddr_read(idt_base + NO * 64 + 48, 2) << 16);
 	printf("offset 0x%-8x\n", offset);
