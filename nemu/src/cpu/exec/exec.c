d4dc34ce0fedda4c96e78513663e9182cd863176
tracer-ics2017 <tracer@njuics.org>
2017-10-03 08:27:57 +0000

    >  compile 201728013229028 kira Linux f082570d4803 4.4.0-96-generic #119-Ubuntu SMP Tue Sep 12 14:59:54 UTC 2017 x86_64 GNU/Linux  08:27:57 up  1:36, 13 users,  load average: 0.02, 0.09, 0.08 7073eac5863f9ea56b8adea0edf9b638fdb02835

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -226,6 +226,7 @@ static inline void update_eip(void) {
 void exec_wrapper(bool print_flag) {
 #ifdef DEBUG
   decoding.p = decoding.asm_buf;
+  printf("%s\n", decoding.asm_buf);
   decoding.p += sprintf(decoding.p, "%8x:   ", cpu.eip);
 #endif
 
